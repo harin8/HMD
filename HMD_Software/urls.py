211422a076872cbbb1a0f493bd2406dd34d0703e
Harin <harin.desai96@gmail.com>
2021-05-22 02:11:25 +0530

    Update urls.py

--- HMD_Software/urls.py
+++ HMD_Software/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('', include('IT_Return.urls')),
     path('certificates/', include('certificates.urls')),
     path('admin/', admin.site.urls),
-    path('clients/', include('clients.urls'))
+    path('clients/', include('clients.urls')),
+    path('contacts/', include('contacts.urls'))
 ]
